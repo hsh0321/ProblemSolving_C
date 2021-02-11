60eb844f857235124e76c3a284ca46c3ec89c666
hsh0321 <ppko1233@gmail.com>
2021-02-11 18:25:27 +0900

    docs : rename file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int N;
+    cin >> N;
+    int cnt = 0;
+    for(;;){
+        if(N == 1)break;
+        if(N % 3 == 0)N /= 3;
+        else if(N % 2 ==0){
+            if((N-1)%3 == 0){
+                N--;
+                cnt++;
+                continue;
+            }
+            N /= 2;
+        }
+        else N--;
+        cnt++;
+    }
+    cout << cnt;
+
 }
